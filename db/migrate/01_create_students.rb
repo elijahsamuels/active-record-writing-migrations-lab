237991c21bfa2df1756a9f9e2276d2bb774dcf64
elijahsamuels <elijahsamuels@gmail.com>
2020-12-14 17:03:47 -0700

    commit tree 3005ad47b1f54b018dfe4797f0ab5c861a7ab27c

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,15 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+    # write the code to create a table with Active Record
+    # Define a method called change and use the Active Record create_table method within that method to create the table. The table should have a :name column with a type string.
+    # After you finish defining the change method, run the migrations by running rake db:migrate in your terminal.
+
+    def change
+        create_table :students do |t|
+        t.string :name
+        end
+    end
+
+
+
 end
